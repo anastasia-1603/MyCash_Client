3144eab22da8c41152fa73289d6aa2f92337aee7
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-16 14:59:18 +0300

    Добавлен стартовый экран

---
+++ app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/StartActivity.kt
@@ -0,0 +1,12 @@
+package cs.vsu.ru.mycash.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.vsu.ru.mycash.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
--- app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/WelcomeActivity.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/WelcomeActivity.kt
@@ -1,12 +1,20 @@
 package cs.vsu.ru.mycash.ui.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import cs.vsu.ru.mycash.R
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@WelcomeActivity, StartActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
