6c742d8d8c11aa0c8f364cfaab2d01c42c64f12e
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-16 14:49:47 +0300

    Добавлен приветственный экран

---
+++ app/src/main/java/cs/vsu/ru/mycash/ui/start/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package cs.vsu.ru.mycash.ui.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.vsu.ru.mycash.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
