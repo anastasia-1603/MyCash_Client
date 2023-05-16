790d376f9f5142db823056824cc1a4ef8db98676
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-16 21:32:21 +0300

    Добавлены activity и layout для главного экрана

---
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package cs.vsu.ru.mycash.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.vsu.ru.mycash.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
