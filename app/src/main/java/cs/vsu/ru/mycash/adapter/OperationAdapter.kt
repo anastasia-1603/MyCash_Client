f52ae8e0941b0de291f156ea4b6bbfd7eff311fc
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-17 03:36:40 +0300

    Добавлен адаптер для операции

---
+++ app/src/main/java/cs/vsu/ru/mycash/adapter/OperationAdapter.kt
@@ -0,0 +1,54 @@
+package cs.vsu.ru.mycash.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.data.Operation
+import cs.vsu.ru.mycash.databinding.ItemOperationBinding
+
+class OperationAdapter : RecyclerView.Adapter<OperationAdapter.OperationViewHolder>() {
+
+    var data: List<Operation> = emptyList()
+    set(newValue) {
+        field = newValue
+        notifyDataSetChanged()
+    }
+
+    class OperationViewHolder(val binding: ItemOperationBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): OperationAdapter.OperationViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemOperationBinding.inflate(inflater, parent, false)
+
+        return OperationViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: OperationAdapter.OperationViewHolder, position: Int) {
+        val operation = data[position] // Получение человека из списка данных по позиции
+        val context = holder.itemView.context
+        val colors = mapOf("Транспорт" to R.color.purple_500,
+            "Еда" to R.color.teal_700, "Одежда" to R.color.red)
+
+        with (holder.binding) {
+            val color = colors[operation.category]
+
+            categoryName.text = operation.category
+            value.text = operation.value.toString()
+            time.text = operation.date.toString()
+            color?.let { ContextCompat.getColor(context, it) }?.let {
+                imageView.setColorFilter(
+                    it,
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = data.size
+}
