175c3abdda9056b59d8dc2e18c9e10c7da11939a
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-17 23:14:19 +0300

    Добавлено нижнее меню

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/MainScreenActivity.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/MainScreenActivity.kt
@@ -1,14 +1,18 @@
 package cs.vsu.ru.mycash.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import cs.vsu.ru.mycash.R
 import cs.vsu.ru.mycash.adapter.OperationAdapter
 import cs.vsu.ru.mycash.databinding.ActivityMainScreenBinding
 import cs.vsu.ru.mycash.service.OperationService
-import cs.vsu.ru.mycash.service.OperationServiceApp
+import cs.vsu.ru.mycash.ui.main.accounts.AccountsFragment
+import cs.vsu.ru.mycash.ui.main.categories.CategoriesFragment
+import cs.vsu.ru.mycash.ui.main.diagrams.DiagramsFragment
+import cs.vsu.ru.mycash.ui.main.home.HomeFragment
+import cs.vsu.ru.mycash.ui.main.profile.ProfileFragment
 
 class MainScreenActivity : AppCompatActivity() {
 
@@ -31,5 +35,52 @@ class MainScreenActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = manager
         binding.recyclerView.adapter = adapter
 
+
+        binding.navView.selectedItemId = R.id.nav_host_fragment_activity_navigation
+        binding.navView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.navigation_diagrams -> {
+                    openFragment(DiagramsFragment())
+                }
+                R.id.navigation_accounts -> {
+                    openFragment(AccountsFragment())
+
+                }
+                R.id.navigation_categories -> {
+                    openFragment(CategoriesFragment())
+                }
+                R.id.navigation_profile -> {
+                    openFragment(ProfileFragment())
+                }
+                R.id.navigation_home->{
+                    openFragment(HomeFragment())
+                }
+            }
+            true
+        }
+//
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_navigation) as NavHostFragment
+//        val navController = navHostFragment.navController
+//
+//
+//        val navView: BottomNavigationView = binding.navView
+//
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.homeFragment, R.id.accountsFragment, R.id.categoriesFragment,
+//                R.id.diagramsFragment, R.id.profileFragment
+//            )
+//        )
+//        navView.setupWithNavController(navController)
+
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.nav_view, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
