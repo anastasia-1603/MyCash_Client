98543f31a7dd109f80adbfd9ef88381b8e736090
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-17 17:31:53 +0300

    Добавлено ограничение длины в названии счета и проверка полей на empty

--- app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/StartActivity.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/StartActivity.kt
@@ -1,10 +1,13 @@
 package cs.vsu.ru.mycash.ui.onboarding
 
 import android.content.Intent
+import android.icu.util.Output
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.LayoutInflater
 import android.widget.Button
+import com.github.javafaker.Bool
 import cs.vsu.ru.mycash.R
 import cs.vsu.ru.mycash.databinding.ActivityStartBinding
 import cs.vsu.ru.mycash.ui.main.MainScreenActivity
@@ -17,14 +20,31 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val balance = binding.editTextBalance
+        val accountName = binding.editTextAccountName
 
         binding.btnContinue.setOnClickListener {
-            val intent = Intent(this@StartActivity, MainScreenActivity::class.java)
-            intent.putExtra("accountName", binding.editTextAccountName.text.toString())
-            intent.putExtra("balance", binding.editTextBalance.text.toString())
-            startActivity(intent)
+            var valid :Boolean = true
+            if (balance.text.trim().isEmpty())
+            {
+                balance.error = "Введите остаток"
+                valid = false
+            }
+            if (accountName.text.trim().isEmpty())
+            {
+                accountName.error = "Введите название"
+                valid = false
+            }
+            if (valid)
+            {
+                val intent = Intent(this@StartActivity, MainScreenActivity::class.java)
+                intent.putExtra("accountName", binding.editTextAccountName.text.toString())
+                intent.putExtra("balance", binding.editTextBalance.text.toString())
+                startActivity(intent)
+            }
+
         }
 
     }
+
 }
