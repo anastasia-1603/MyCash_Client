4acfbaaeb972d4d709a5799475d6b1f89140d4e7
vladagaraba <vladislava.garaba@mail.ru>
2023-05-18 21:53:05 +0300

    Добавлен экран авторизации

---
+++ app/src/main/java/cs/vsu/ru/mycash/ui/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package cs.vsu.ru.mycash.ui.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
