6f5eea58ddb31065cc60a433e246132e7a9ed8e7
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-20 01:39:32 +0300

    Добавлены константы для работы с api

--- app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
+++ app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
@@ -3,5 +3,6 @@ package cs.vsu.ru.mycash.api
 object Constants {
     // Endpoints
     const val BASE_URL = "https://6d79-77-45-134-18.ngrok-free.app/api/"
-    const val LOGIN_URL = "user"
+    const val TOKEN_URL = "user"
+    const val INIT_URL = ""
 }
