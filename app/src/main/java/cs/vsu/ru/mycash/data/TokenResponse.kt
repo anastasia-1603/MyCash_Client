ac17a3baef1da3e24eaa7c93810f9dadff06345e
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-20 22:06:22 +0300

    Изменено имя для сериализации

--- app/src/main/java/cs/vsu/ru/mycash/data/TokenResponse.kt
+++ app/src/main/java/cs/vsu/ru/mycash/data/TokenResponse.kt
@@ -3,6 +3,6 @@ package cs.vsu.ru.mycash.data
 import com.google.gson.annotations.SerializedName
 
 data class TokenResponse(
-    @SerializedName("access_token")
+    @SerializedName("token")
     val token: String
 )
