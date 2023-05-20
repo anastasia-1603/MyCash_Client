fc6b42f99df6dd3ba53e9acd3ccff40d95bd93a3
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-20 13:57:36 +0300

    Перенесено отправление запроса в StartActivity

--- app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/StartActivity.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/onboarding/StartActivity.kt
@@ -3,15 +3,30 @@ package cs.vsu.ru.mycash.ui.onboarding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.Button
-import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.ui.auth.AuthViewModel
+import cs.vsu.ru.mycash.ui.auth.TokenViewModel
+
+import dagger.hilt.android.AndroidEntryPoint
+import cs.vsu.ru.mycash.api.TokenApiService
 import cs.vsu.ru.mycash.databinding.ActivityStartBinding
 import cs.vsu.ru.mycash.ui.main.MainScreenActivity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class StartActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityStartBinding
+    private val viewModel: AuthViewModel by viewModels()
+    private val tokenViewModel: TokenViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +35,20 @@ class StartActivity : AppCompatActivity() {
 
 
         binding.btnContinue.setOnClickListener {
+
+
+//            val tokenApiService = ApiClient.getClient(token).create(TokenApiService::class.java)
+//            val call = tokenApiService.getToken()
+//
+//            call.enqueue(object : Callback<String> {
+//                override fun onResponse(call: Call<String>, response: Response<String>) {
+//                }
+//
+//                override fun onFailure(call: Call<String>, t: Throwable) {
+//                    // Обработка ошибки при отправке запроса
+//                }
+//            })
+
             val intent = Intent(this@StartActivity, MainScreenActivity::class.java)
             intent.putExtra("accountName", binding.editTextAccountName.text.toString())
             intent.putExtra("balance", binding.editTextBalance.text.toString())
