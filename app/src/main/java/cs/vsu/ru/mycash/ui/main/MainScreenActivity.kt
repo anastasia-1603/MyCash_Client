a3a87fbc6062633bc2964521ec6a77328a08832d
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-20 18:00:05 +0300

    Добавлена зависимость для навигации

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/MainScreenActivity.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/MainScreenActivity.kt
@@ -2,22 +2,11 @@ package cs.vsu.ru.mycash.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import cs.vsu.ru.mycash.R
-import cs.vsu.ru.mycash.adapter.OperationAdapter
 import cs.vsu.ru.mycash.databinding.ActivityMainScreenBinding
-import cs.vsu.ru.mycash.service.OperationService
-import cs.vsu.ru.mycash.ui.main.accounts.AccountsFragment
-import cs.vsu.ru.mycash.ui.main.categories.CategoriesFragment
-import cs.vsu.ru.mycash.ui.main.diagrams.DiagramsFragment
-import cs.vsu.ru.mycash.ui.main.home.HomeFragment
-import cs.vsu.ru.mycash.ui.main.profile.ProfileFragment
-
 class MainScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainScreenBinding
