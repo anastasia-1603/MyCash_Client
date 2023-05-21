1e97964a4ab2558458c3a550c50c040cf588b958
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-21 23:37:34 +0300

    Исправлены тестовые данные

--- app/src/main/java/cs/vsu/ru/mycash/service/OperationService.kt
+++ app/src/main/java/cs/vsu/ru/mycash/service/OperationService.kt
@@ -1,9 +1,15 @@
 package cs.vsu.ru.mycash.service
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.github.javafaker.Faker
+import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.data.Account
+import cs.vsu.ru.mycash.data.Category
+import cs.vsu.ru.mycash.data.CategoryType
 import cs.vsu.ru.mycash.data.Operation
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -15,16 +21,34 @@ class OperationService {
 
     var operations = mutableListOf<Operation>()
 
+
     init {
-        val categories = listOf("Транспорт", "Еда", "Одежда")
-        val names = listOf("Новый счет", "Сбережения", "Отпуск")
+        val category = Category(1,
+            "Транспорт",
+            Color.GREEN,
+            CategoryType.ALL,
+            false, 0.0)
+        val category2 = Category(2,
+            "Еда",
+            Color.CYAN,
+            CategoryType.ALL,
+            false, 0.0)
+        val category3 = Category(3,
+            "Развлечения",
+            Color.MAGENTA,
+            CategoryType.ALL,
+            false, 0.0)
+        val account = Account(1, "Новый счет", 1000.0, 10.0, 10000.0)
+//        val account2 = Account(2, "Второй счет", 1000.0, 10.0, 10000.0)
+
+        val categories = listOf(category, category2, category3)
 
         operations = (1..10).map {
             Operation(
                 id = it.toLong(),
-                account = names[Random.nextInt(names.size)],
+                account = account,
                 category = categories[Random.nextInt(categories.size)],
-                date = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm")),
+                date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm")),
                 value = Random.nextInt(10000).toDouble()
             )
         }.toMutableList()
