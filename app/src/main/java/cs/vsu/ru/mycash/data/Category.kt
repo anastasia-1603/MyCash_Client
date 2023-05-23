6297bbe276a91f1aad43a463fcbd263f72d6c062
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-05-24 02:23:07 +0300

    Изменен тип поля color с Int на Color

--- app/src/main/java/cs/vsu/ru/mycash/data/Category.kt
+++ app/src/main/java/cs/vsu/ru/mycash/data/Category.kt
@@ -6,7 +6,7 @@ import android.graphics.drawable.ColorDrawable
 data class Category (
     val id: Long,
     val name: String,
-    val color: Int,
+    val color: Color,
     val type: CategoryType,
     val isLimited: Boolean,
     val spendingLimit: Double
