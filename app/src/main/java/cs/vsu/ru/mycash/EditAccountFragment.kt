04c02eb56721681edd8a627e5d84cec38b97bb48
vladagaraba <vladislava.garaba@mail.ru>
2023-05-31 23:03:41 +0300

    Добавлены классы и разметки для редактирования счета

---
+++ app/src/main/java/cs/vsu/ru/mycash/EditAccountFragment.kt
@@ -0,0 +1,31 @@
+package cs.vsu.ru.mycash
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EditAccountFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = EditAccountFragment()
+    }
+
+    private lateinit var viewModel: EditAccountViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_account, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditAccountViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/cs/vsu/ru/mycash/EditAccountViewModel.kt
@@ -0,0 +1,7 @@
+package cs.vsu.ru.mycash
+
+import androidx.lifecycle.ViewModel
+
+class EditAccountViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
