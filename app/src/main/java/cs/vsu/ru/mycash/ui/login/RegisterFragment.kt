00f0742729eb40993fe8a36a85b8ef45e13cb221
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-01 02:53:40 +0300

    Добавлен фрагмент для регистрации

--- app/src/main/java/cs/vsu/ru/mycash/ui/login/RegisterFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/login/RegisterFragment.kt
@@ -1,60 +1,87 @@
 package cs.vsu.ru.mycash.ui.login
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import cs.vsu.ru.mycash.R
+import androidx.lifecycle.ViewModelProvider
+import cs.vsu.ru.mycash.api.ApiClient
+import cs.vsu.ru.mycash.api.ApiService
+import cs.vsu.ru.mycash.data.RegisterRequest
+import cs.vsu.ru.mycash.data.TokenResponse
+import cs.vsu.ru.mycash.databinding.FragmentRegisterBinding
+import cs.vsu.ru.mycash.utils.AppPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentRegisterBinding? = null
+    private lateinit var appPrefs: AppPreferences
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        val viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        appPrefs = activity?.let { AppPreferences(it) }!!
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        val username = binding.username
+        val password = binding.password
+
+        binding.continueBtn.setOnClickListener {
+            var valid = true
+            if (username.text.trim().isEmpty()) {
+                username.error = "Введите логин"
+                valid = false
+            }
+            if (password.text.trim().isEmpty()) {
+                password.error = "Введите пароль"
+            }
+
+            if (valid) {
+                val apiService = ApiClient.initClient().create(ApiService::class.java)
+                val call = apiService.register(
+                    RegisterRequest(
+                        username.text.toString(),
+                        password.text.toString()
+                    )
+                )
+
+                call.enqueue(object : Callback<TokenResponse> {
+                    override fun onResponse(
+                        call: Call<TokenResponse>,
+                        response: Response<TokenResponse>
+                    ) {
+                        val preferences =
+                            activity?.getSharedPreferences("MY_APP", Context.MODE_PRIVATE)
+                        val token = response.body()?.token ?: "-1"
+                        preferences?.edit()?.putString("TOKEN", token)?.apply()
+                        appPrefs.isAuth = true
+                    }
+
+                    override fun onFailure(call: Call<TokenResponse>, t: Throwable) {
+                        t.message?.let { Log.e("failure register", it) }
+                    }
+
+                })
+            }
+        }
+
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
