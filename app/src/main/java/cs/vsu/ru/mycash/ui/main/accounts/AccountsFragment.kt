43c09925daaa1e169ad31d5202029e6e321ead26
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-02 18:16:20 +0300

    Исправлен код

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/accounts/AccountsFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/accounts/AccountsFragment.kt
@@ -12,8 +12,6 @@ import cs.vsu.ru.mycash.databinding.FragmentAccountsBinding
 class AccountsFragment : Fragment() {
     private var _binding: FragmentAccountsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,15 +20,14 @@ class AccountsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val accountsViewModel =
-            ViewModelProvider(this).get(AccountsViewModel::class.java)
+            ViewModelProvider(this)[AccountsViewModel::class.java]
 
         _binding = FragmentAccountsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAccounts
-        accountsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
+
+
         return root
     }
 
