d1c3db65eb7bd1ccb095023b366cad7b0648f10b
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-04 01:09:35 +0300

    Изменен val на var

--- app/src/main/java/cs/vsu/ru/mycash/data/Category.kt
+++ app/src/main/java/cs/vsu/ru/mycash/data/Category.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 
 data class Category (
-    val name: String,
+    var name: String,
     val color: Int,
     val type: CategoryType,
     val isLimited: Boolean,
