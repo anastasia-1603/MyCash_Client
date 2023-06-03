7d77c9277d9572d07b37abf69bf415d7e1b15e37
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-04 01:18:02 +0300

    Вместо isOverLimited добавлен LimitType.None

--- app/src/main/java/cs/vsu/ru/mycash/data/OperationResponse.kt
+++ app/src/main/java/cs/vsu/ru/mycash/data/OperationResponse.kt
@@ -1,11 +1,12 @@
 package cs.vsu.ru.mycash.data
 
 data class OperationResponse(
-    val isOverLimit: Boolean,
     val type: LimitType
 )
 
 enum class LimitType {
+    NONE,
     CATEGORY,
     ACCOUNT
+
 }
