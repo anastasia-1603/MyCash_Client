a9e1c78640ed1059ceb714fc926e3b9fe3ae03d3
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-04 01:12:38 +0300

    Добавлен список счетов и категорий

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/operation/AddOperationViewModel.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/operation/AddOperationViewModel.kt
@@ -3,6 +3,8 @@ package cs.vsu.ru.mycash.ui.main.operation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cs.vsu.ru.mycash.data.Account
+import cs.vsu.ru.mycash.data.Category
 import java.util.*
 
 class AddOperationViewModel : ViewModel() {
@@ -15,6 +17,34 @@ class AddOperationViewModel : ViewModel() {
         MutableLiveData<String>()
     }
     val sum: LiveData<String> = _sum
+
+    private val _accounts : MutableLiveData<List<Account>> by lazy {
+        MutableLiveData<List<Account>>()
+    }
+    val accounts : LiveData<List<Account>> = _accounts
+
+    fun setAccounts(accounts: List<Account>) {
+        _accounts.value = accounts
+    }
+
+    private val _categoryName : MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val categoryName : LiveData<String> = _categoryName
+
+    fun setCategoryName(categoryName: String) {
+        _categoryName.value = categoryName
+    }
+
+    private val _categories : MutableLiveData<List<Category>> by lazy {
+        MutableLiveData<List<Category>>()
+    }
+    val categories : LiveData<List<Category>> = _categories
+
+    fun setCategories(categories: List<Category>) {
+        _categories.value = categories
+    }
+
 
     private val _date: MutableLiveData<Calendar> by lazy {
         MutableLiveData<Calendar>(Calendar.getInstance())
