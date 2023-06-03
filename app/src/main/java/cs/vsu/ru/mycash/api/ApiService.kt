20ceda43d6ca1287d30d2545744226ed70128e60
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-03 19:56:11 +0300

    Добавлены эндпойнты

--- app/src/main/java/cs/vsu/ru/mycash/api/ApiService.kt
+++ app/src/main/java/cs/vsu/ru/mycash/api/ApiService.kt
@@ -1,28 +1,32 @@
 package cs.vsu.ru.mycash.api
 
 
-import android.service.autofill.UserData
 import cs.vsu.ru.mycash.api.Constants.ACCOUNT_URL
 import cs.vsu.ru.mycash.api.Constants.CATEGORY_URL
 import cs.vsu.ru.mycash.api.Constants.INFO
+import cs.vsu.ru.mycash.api.Constants.DATA_DAY
+import cs.vsu.ru.mycash.api.Constants.DATA_MONTH
 import cs.vsu.ru.mycash.api.Constants.INIT_URL
 import cs.vsu.ru.mycash.api.Constants.LOGIN
 import cs.vsu.ru.mycash.api.Constants.REGISTER
 import cs.vsu.ru.mycash.data.*
 import retrofit2.Call
 import retrofit2.http.*
-import java.time.LocalDateTime
 
 interface ApiService {
     @POST(INIT_URL)
     fun init(@Body accountInit: AccountInit): Call<TokenResponse>
 
-    @GET(INFO)
-    fun getAccountInfo(@Path("account") account: String,
-                       @Path("year") year: Int,
-                       @Path("month") month: Int,
-                       @Path("day") day: Int) :
-            Call<List<Operation>>
+    @GET(DATA_DAY)
+    fun getDataByDay(@Path("year") year: Int,
+                     @Path("month") month: Int,
+                     @Path("day") day: Int) :
+            Call<Map<String, List<Operation>>>
+
+    @GET(DATA_MONTH)
+    fun getDataByMonth(@Path("year") year: Int,
+                       @Path("month") month: Int,) :
+            Call<Map<String, List<Operation>>>
 
     @POST(REGISTER)
     fun register(@Body registerRequest: RegisterRequest) : Call<TokenResponse>
