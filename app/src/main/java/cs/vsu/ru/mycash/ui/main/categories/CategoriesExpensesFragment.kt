1aaf2b9f716a14dabdd6e2dbbe8576ac5d958165
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-03 20:06:41 +0300

    Исправлено название списка

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesExpensesFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesExpensesFragment.kt
@@ -1,14 +1,11 @@
 package cs.vsu.ru.mycash.ui.main.categories
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import cs.vsu.ru.mycash.databinding.FragmentCategoriesExpensesBinding
 
@@ -32,8 +29,8 @@ class CategoriesExpensesFragment : Fragment() {
         val expenseCategoriesList = categoriesViewModel.expenseCategories.value
         adapter.data = expenseCategoriesList ?: emptyList()
 
-        binding.recyclerView2.layoutManager = manager
-        binding.recyclerView2.adapter = adapter
+        binding.recyclerViewExpenses.layoutManager = manager
+        binding.recyclerViewExpenses.adapter = adapter
 
 
         categoriesViewModel.expenseCategories.observe(viewLifecycleOwner) {
--- app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesIncomeFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesIncomeFragment.kt
@@ -1,16 +1,12 @@
 package cs.vsu.ru.mycash.ui.main.categories
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import cs.vsu.ru.mycash.R
 import cs.vsu.ru.mycash.databinding.FragmentCategoriesIncomeBinding
 
 class CategoriesIncomeFragment : Fragment() {
@@ -33,8 +29,8 @@ class CategoriesIncomeFragment : Fragment() {
         val incomeCategoriesList = categoriesViewModel.incomeCategories.value
         adapter.data = incomeCategoriesList ?: emptyList()
 
-        binding.recyclerView2.layoutManager = manager
-        binding.recyclerView2.adapter = adapter
+        binding.recyclerViewIncome.layoutManager = manager
+        binding.recyclerViewIncome.adapter = adapter
 
 
         categoriesViewModel.incomeCategories.observe(viewLifecycleOwner) {
