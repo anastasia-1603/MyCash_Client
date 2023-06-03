8dab131d722e3cbfcaca0a9ae712fee562f5b164
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-03 18:33:19 +0300

    Добавлены url для получения данных по месяцам

--- app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
+++ app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
@@ -1,13 +1,14 @@
 package cs.vsu.ru.mycash.api
 
 object Constants {
-    const val BASE_URL = "https://cb8e-95-32-198-80.ngrok-free.app/api/"
-    const val TOKEN_URL = "user"
+    const val BASE_URL = "https://6166-95-32-198-80.ngrok-free.app/api/"
     const val INIT_URL = "auth/new"
     const val LOGIN = "auth/login"
     const val REGISTER = "auth/register"
     const val CATEGORY_URL = "category/get_all"
     const val INFO = "main/get/{account}/{year}/{month}/{day}"
+    const val DATA_DAY = "main/get/{year}/{month}/{day}"
     const val ACCOUNT_URL = "account/get_all"
+    const val DATA_MONTH = "main/get/{year}/{month}"
 
 }
