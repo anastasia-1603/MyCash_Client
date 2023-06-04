3da6e3903cbe6c48d0b7f2caa38482a3b9f7e2e4
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-04 15:53:55 +0300

    Добавлены классы для диаграмм

---
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/diagrams/DiagramsAllViewModel.kt
@@ -0,0 +1,7 @@
+package cs.vsu.ru.mycash.ui.main.diagrams
+
+import androidx.lifecycle.ViewModel
+
+class DiagramsAllViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/cs/vsu/ru/mycash/ui/main/diagrams/DiagramsIncomeFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/diagrams/DiagramsIncomeFragment.kt
@@ -1,6 +1,32 @@
 package cs.vsu.ru.mycash.ui.main.diagrams
 
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cs.vsu.ru.mycash.R
 
-class DiagramsIncomeFragment : Fragment()  {
+class DiagramsIncomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DiagramsIncomeFragment()
+    }
+
+    private lateinit var viewModel: DiagramsIncomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_diagrams_income, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DiagramsIncomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
 }
