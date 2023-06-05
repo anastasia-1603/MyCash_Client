8266cc579222b7ee43758e630b66cbc932dc0c5e
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-05 17:58:27 +0300

    Изменен url сервера

--- app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
+++ app/src/main/java/cs/vsu/ru/mycash/api/Constants.kt
@@ -1,7 +1,7 @@
 package cs.vsu.ru.mycash.api
 
 object Constants {
-    const val BASE_URL = "https://44f9-95-32-198-80.ngrok-free.app/api/"
+    const val BASE_URL = "http://45.140.168.75:4080/api/"
     const val INIT_URL = "auth/new"
     const val LOGIN = "auth/login"
     const val REGISTER = "auth/register"
