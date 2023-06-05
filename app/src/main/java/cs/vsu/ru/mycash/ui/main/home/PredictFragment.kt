95f6720ff6d2b075cd9a06465245c01179d09afe
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-05 13:21:03 +0300

    Добавлен фрагмент предсказаний

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/home/PredictFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/home/PredictFragment.kt
@@ -1,32 +1,241 @@
 package cs.vsu.ru.mycash.ui.main.home
 
-import androidx.lifecycle.ViewModelProvider
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.api.ApiClient
+import cs.vsu.ru.mycash.api.ApiService
+import cs.vsu.ru.mycash.data.AccountDto
+import cs.vsu.ru.mycash.data.Operation
+import cs.vsu.ru.mycash.data.PredictionResponse
+import cs.vsu.ru.mycash.databinding.FragmentPredictBinding
+import cs.vsu.ru.mycash.utils.AppPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 class PredictFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PredictFragment()
-    }
+    private var _binding: FragmentPredictBinding? = null
+    private var cal = Calendar.getInstance()
+    private val dateFormat = "d MMMM"
+    private lateinit var predictViewModel: PredictViewModel
+    private val homeViewModel :HomeViewModel by activityViewModels()
+    private val binding get() = _binding!!
 
-    private lateinit var viewModel: PredictViewModel
+    private lateinit var apiService: ApiService
+    private lateinit var appPrefs: AppPreferences
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_predict, container, false)
+    ): View {
+        appPrefs = activity?.let { AppPreferences(it) }!!
+        _binding = FragmentPredictBinding.inflate(inflater, container, false)
+        predictViewModel = ViewModelProvider(this)[PredictViewModel::class.java]
+        homeViewModel.accountList.value?.let { predictViewModel.setAccountsList(it) }
+        homeViewModel.date.value?.let { predictViewModel.setDate(it) }
+        homeViewModel.accountName.value?.let { predictViewModel.setAccountName(it) }
+        val root: View = binding.root
+
+        val accountName: TextView = binding.accountName
+        predictViewModel.accountName.observe(viewLifecycleOwner) {
+            accountName.text = it
+        }
+
+        val balance: TextView = binding.balancePredict
+        predictViewModel.balance.observe(viewLifecycleOwner) {
+            balance.text = it
+        }
+        val dateBtn: Button = binding.date
+
+        predictViewModel.accountIndex.observe(viewLifecycleOwner) {
+            val accounts = predictViewModel.accountList.value
+            val index = it
+            if (index != null) {
+                val account = accounts?.get(index)
+                if (account != null) {
+                    predictViewModel.setAccountName(account.name.toString())
+                    getPredict()
+                }
+            }
+        }
+
+        predictViewModel.date.observe(viewLifecycleOwner) {
+            val cur = Calendar.getInstance()
+            if (it.get(Calendar.YEAR) != cur.get(Calendar.YEAR) ||
+                    it.get(Calendar.MONTH) > cur.get(Calendar.MONTH) + 1)
+            {
+                findNavController().navigate(R.id.homeFragment)
+            }
+            else {
+                val monthNames = arrayOf(
+                    "Январь",
+                    "Февраль",
+                    "Март",
+                    "Апрель",
+                    "Май",
+                    "Июнь",
+                    "Июль",
+                    "Август",
+                    "Сентябрь",
+                    "Октябрь",
+                    "Ноябрь",
+                    "Декабрь"
+                )
+
+                dateBtn.text = monthNames[cal.get(Calendar.MONTH)]
+            }
+
+        }
+
+
+
+        binding.date.setOnClickListener {
+            val dialog = datePickerDialog()
+            dialog?.show()
+            val day = dialog?.findViewById<View>(
+                Resources.getSystem().getIdentifier("android:id/day", null, null)
+            )
+            if (day != null) {
+                day.visibility = View.GONE
+            }
+
+        }
+
+        binding.left.setOnClickListener {
+            findNavController().navigate(R.id.homeFragment)
+        }
+
+        binding.right.isEnabled = false
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.addOperationFragment)
+        }
+
+        if (predictViewModel.accountList.value != null
+            && predictViewModel.accountList.value!!.size > 1
+        ) {
+            binding.leftAccount.isVisible = true
+            binding.rightAccount.isVisible = true
+        } else {
+            binding.leftAccount.isVisible = false
+            binding.rightAccount.isVisible = false
+        }
+
+        binding.leftAccount.setOnClickListener {
+            predictViewModel.decrementAccountIndex()
+        }
+
+        binding.rightAccount.setOnClickListener {
+            predictViewModel.incrementAccountIndex()
+        }
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PredictViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onResume() {
+        super.onResume()
+        getPredict()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun datePickerDialog(): DatePickerDialog? {
+        val c = predictViewModel.date.value ?: Calendar.getInstance()
+
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog =
+            context?.let {
+                DatePickerDialog(
+                    it,
+                    android.R.style.Theme_Holo_Light_Dialog,
+                    { _, year, monthOfYear, dayOfMonth ->
+                        cal.set(Calendar.YEAR, year)
+                        cal.set(Calendar.MONTH, monthOfYear)
+                        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                        predictViewModel.setDate(cal)
+                    },
+                    year,
+                    month,
+                    day
+                )
+            }
+        if (datePickerDialog != null) {
+            val maxC = Calendar.getInstance()
+            val cur = Calendar.getInstance()
+            maxC.set(Calendar.YEAR, cur.get(Calendar.YEAR))
+            maxC.set(Calendar.MONTH, cur.get(Calendar.MONTH) + 1)
+            datePickerDialog.datePicker.maxDate =  maxC.time.time
+        }
+        return datePickerDialog
+    }
+
+    private fun getPredict()
+    {
+        apiService = ApiClient.getClient(appPrefs.token.toString())
+        val date = predictViewModel.date.value
+        if (date != null) {
+            val call: Call<PredictionResponse> =
+                apiService.getPredict(
+                    binding.accountName.text.toString(),
+                    date.get(Calendar.YEAR),
+                    date.get(Calendar.MONTH))
+            call.enqueue(object : Callback<PredictionResponse> {
+                override fun onResponse(
+                    call: Call<PredictionResponse>,
+                    response: Response<PredictionResponse>
+                ) {
+                    val resp = response.body()
+                    if (resp != null)
+                    {
+                        predictViewModel.setCategory(resp.topCategory)
+                        predictViewModel.setPredictExp(resp.expensePrediction)
+                        predictViewModel.setPredictInc(resp.incomePrediction)
+                        predictViewModel.setPredictSum(resp.topCategoryPrediction)
+                        val balance = homeViewModel.balance.value?.toDouble()
+
+                        if (balance != null) {
+                            predictViewModel.setBalance(
+                                balance + resp.incomePrediction - resp.expensePrediction)
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<PredictionResponse>, t: Throwable) {
+                    Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
+
+
     }
 
 }
+
+
--- app/src/main/java/cs/vsu/ru/mycash/ui/main/home/PredictViewModel.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/home/PredictViewModel.kt
@@ -1,7 +1,106 @@
 package cs.vsu.ru.mycash.ui.main.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cs.vsu.ru.mycash.data.Account
+import cs.vsu.ru.mycash.data.Category
+import java.util.*
 
 class PredictViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _accountIndex: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>(0)
+    }
+    val accountIndex: LiveData<Int> = _accountIndex
+
+    fun setAccountIndex(index: Int) {
+        _accountIndex.value = index
+    }
+
+    fun decrementAccountIndex() {
+        _accountIndex.value = _accountIndex.value!! - 1
+    }
+
+    fun incrementAccountIndex() {
+        _accountIndex.value = _accountIndex.value!! + 1
+    }
+
+    private val _accountName: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val accountName: LiveData<String> = _accountName
+
+    private val _accountList: MutableLiveData<List<Account>> by lazy {
+        MutableLiveData<List<Account>>()
+    }
+    val accountList : LiveData<List<Account>> = _accountList
+
+    fun setAccountsList(accounts: List<Account>)
+    {
+        _accountList.value = accounts
+    }
+
+    private val _balance: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val balance: LiveData<String> = _balance
+
+    private val _date: MutableLiveData<Calendar> by lazy {
+        MutableLiveData<Calendar>(Calendar.getInstance())
+    }
+    val date: LiveData<Calendar> = _date
+
+
+    fun setDate(date: Calendar) {
+        _date.value = date
+    }
+
+    fun setAccountName(accountName: String) {
+        _accountName.value = accountName
+    }
+
+    fun setBalance(balance: Double) {
+        _balance.value = balance.toString() + "₽"
+    }
+
+    private val _predictInc: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val predictInc: LiveData<String> = _predictInc
+
+
+    fun setPredictInc(predictInc: Double) {
+        _predictInc.value = predictInc.toString()
+    }
+
+    private val _predictExp: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val predictExp: LiveData<String> = _predictExp
+
+    fun setPredictExp(predictExp: Double) {
+        _predictExp.value = predictExp.toString()
+    }
+
+    private val _category: MutableLiveData<Category> by lazy {
+        MutableLiveData<Category>()
+    }
+    val category: LiveData<Category> = _category
+
+
+    fun setCategory(category: Category) {
+        _category.value = category
+    }
+
+    private val _predictSum: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    val predictSum: LiveData<String> = _predictSum
+
+    fun setPredictSum(predictSum: Double) {
+        _predictSum.value = predictSum.toString()
+    }
+
 }
