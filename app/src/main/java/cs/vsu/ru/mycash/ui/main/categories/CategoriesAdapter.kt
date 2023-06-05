a7829e95103c42df5b4d69b71f2962c2bd7ff9b6
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-05 17:57:38 +0300

    Исправлена ошибка с цветом

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesAdapter.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesAdapter.kt
@@ -40,7 +40,7 @@ class CategoriesAdapter(private val onClickListener: CategoriesAdapter.OnClickLi
         with (holder.binding) {
 
             categoryName.text = category.name
-            imageView.setColorFilter(category.color)
+            imageView.setColorFilter(-category.color)
 
             holder.itemView.setOnClickListener {
                 onClickListener.onClick(category)
