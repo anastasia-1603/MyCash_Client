d5bf9e34658712b523808ac87ccae9fbe89da84d
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-08 11:14:23 +0300

    Подключена библиотека AppMetrica SDK

---
+++ app/src/main/java/cs/vsu/ru/mycash/utils/MyApp.kt
@@ -0,0 +1,17 @@
+package cs.vsu.ru.mycash.utils
+
+import android.app.Application
+import com.yandex.metrica.YandexMetrica
+import com.yandex.metrica.YandexMetricaConfig
+
+class MyApp : Application() {
+
+    private val API_key = "ec6eff70-af6c-4f05-80a2-33d40914193c"
+    override fun onCreate() {
+        super.onCreate()
+        val config = YandexMetricaConfig.newConfigBuilder(API_key).build()
+        YandexMetrica.activate(applicationContext, config)
+        YandexMetrica.enableActivityAutoTracking(this)
+
+    }
+}
