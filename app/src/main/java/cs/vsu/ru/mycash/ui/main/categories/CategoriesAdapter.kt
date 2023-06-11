91abf082f8206b94b91b8f92b2fdef2e4d90aba9
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-11 12:34:03 +0300

    Оптимизированы импорты

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesAdapter.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/categories/CategoriesAdapter.kt
@@ -6,11 +6,7 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import cs.vsu.ru.mycash.data.Category
-import cs.vsu.ru.mycash.data.Operation
-import cs.vsu.ru.mycash.databinding.FragmentCategoriesBinding
 import cs.vsu.ru.mycash.databinding.ItemCategoryBinding
-import cs.vsu.ru.mycash.databinding.ItemOperationBinding
-import cs.vsu.ru.mycash.utils.OperationAdapter
 
 class CategoriesAdapter(private val onClickListener: CategoriesAdapter.OnClickListener)
     : RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>() {
