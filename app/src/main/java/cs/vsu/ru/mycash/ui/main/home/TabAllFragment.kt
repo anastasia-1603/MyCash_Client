9ef6fd55da9e5a16f00ec0670bfa0b28f0e8b0e0
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-11 12:23:38 +0300

    Добавлен переход на экран редактирования операции

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/home/TabAllFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/home/TabAllFragment.kt
@@ -1,23 +1,29 @@
 package cs.vsu.ru.mycash.ui.main.home
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import cs.vsu.ru.mycash.utils.OperationAdapter
+import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.ui.main.operation.OperationAdapter
 import cs.vsu.ru.mycash.databinding.FragmentTabAllBinding
+import cs.vsu.ru.mycash.ui.main.operation.EditOperationViewModel
+import cs.vsu.ru.mycash.ui.main.operation.OperationViewModel
 
 
 class TabAllFragment : Fragment() {
     private lateinit var binding: FragmentTabAllBinding
     private lateinit var adapter: OperationAdapter
     private val operationViewModel: OperationViewModel by activityViewModels()
+    private val editOperationViewModel : EditOperationViewModel by activityViewModels()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,8 +32,10 @@ class TabAllFragment : Fragment() {
         binding = FragmentTabAllBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val manager = LinearLayoutManager(context)
+
         adapter = OperationAdapter(OperationAdapter.OnClickListener { operation ->
-            Toast.makeText(activity, operation.comment, Toast.LENGTH_SHORT).show()
+            editOperationViewModel.setOperation(operation)
+            findNavController().navigate(R.id.editOperationFragment)
         })
 
         val operationList = operationViewModel.operationList.value
@@ -42,7 +50,5 @@ class TabAllFragment : Fragment() {
 
         return root
     }
-
-
 
 }
