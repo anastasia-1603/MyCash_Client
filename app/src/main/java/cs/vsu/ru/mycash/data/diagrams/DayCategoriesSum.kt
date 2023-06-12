3372a0c4ad1d158d3415a9d0196022f5837711a8
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-12 20:13:04 +0300

    Изменена структура получаемых данных

--- app/src/main/java/cs/vsu/ru/mycash/data/diagrams/DayCategoriesSum.kt
+++ app/src/main/java/cs/vsu/ru/mycash/data/diagrams/DayCategoriesSum.kt
@@ -3,6 +3,6 @@ package cs.vsu.ru.mycash.data.diagrams
 import cs.vsu.ru.mycash.data.Category
 
 data class DayCategoriesSum(
-    val category: Category,
-    val data: List<Pair<Int, Double>>
+    val day: Int,
+    val data: List<Pair<Category, Double>>
 )
