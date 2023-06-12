48791bae295bc50da65892b7ee6792dcc00fc5d7
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-12 20:14:24 +0300

    Реализован вывод графика доходов

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/diagrams/DiagramsIncomeViewModel.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/diagrams/DiagramsIncomeViewModel.kt
@@ -1,7 +1,21 @@
 package cs.vsu.ru.mycash.ui.main.diagrams
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cs.vsu.ru.mycash.data.diagrams.DayCategoriesSum
 
 class DiagramsIncomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _dataList: MutableLiveData<List<DayCategoriesSum>> by lazy {
+        MutableLiveData<List<DayCategoriesSum>>()
+    }
+
+    val dataList : LiveData<List<DayCategoriesSum>> = _dataList
+
+    fun setDataList(dataList: List<DayCategoriesSum>)
+    {
+        _dataList.value = dataList
+    }
+
+
 }
