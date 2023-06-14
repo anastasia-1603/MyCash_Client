f2fa34d9afad1ceb285c327b6299bd5313912782
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2023-06-14 11:39:37 +0300

    Добавлено удаление пользователя

--- app/src/main/java/cs/vsu/ru/mycash/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/cs/vsu/ru/mycash/ui/main/profile/ProfileFragment.kt
@@ -9,17 +9,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import cs.vsu.ru.mycash.R
+import cs.vsu.ru.mycash.api.ApiClient
+import cs.vsu.ru.mycash.api.ApiService
 import cs.vsu.ru.mycash.databinding.FragmentProfileBinding
 import cs.vsu.ru.mycash.utils.AppPreferences
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private lateinit var appPrefs: AppPreferences
     private val binding get() = _binding!!
+    private lateinit var apiService: ApiService
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,12 +70,21 @@ class ProfileFragment : Fragment() {
             alertDialogBuilder.setMessage("Вы уверены, что хотите удалить аккаунт?")
 
             alertDialogBuilder.setPositiveButton("Да") { dialog, _ ->
-                appPrefs.isAuth = false
-                appPrefs.token = ""
-                appPrefs.username = ""
-                dialog.dismiss()
-                navController.navigate(R.id.profileFragment)
+                apiService = ApiClient.getClient(appPrefs.token.toString())
+                apiService.delete().enqueue(object : Callback<Void>{
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        appPrefs.isAuth = false
+                        appPrefs.token = ""
+                        appPrefs.username = ""
+                        dialog.dismiss()
+                        navController.navigate(R.id.profileFragment)
+                    }
 
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+                    }
+
+                })
             }
             alertDialogBuilder.setNegativeButton("Нет") { dialog, _ ->
                 dialog.dismiss()
@@ -79,10 +95,8 @@ class ProfileFragment : Fragment() {
         return root
     }
 
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
